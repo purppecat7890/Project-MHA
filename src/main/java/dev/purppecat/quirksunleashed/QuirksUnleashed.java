8498dbd3b1e6cfb5c72ebaec7d58041816281d4b
Admin <roeesalama@gmail.com>
2025-12-04 22:55:07 +0200

    first

--- src/main/java/dev/purppecat/quirksunleashed/QuirksUnleashed.java
+++ src/main/java/dev/purppecat/quirksunleashed/QuirksUnleashed.java
@@ -1,4 +1,4 @@
-package dev.thomasglasser.neoforgetemplate;
+package dev.purppecat.quirksunleashed;
 
 import net.minecraft.resources.ResourceLocation;
 import net.neoforged.bus.api.IEventBus;
@@ -6,13 +6,15 @@ import net.neoforged.fml.common.Mod;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-@Mod(NeoForgeTemplate.MOD_ID)
-public class NeoForgeTemplate {
-    public static final String MOD_ID = "neoforgetemplate";
-    public static final String MOD_NAME = "NeoForge Template";
+@Mod(QuirksUnleashed.MOD_ID)
+public class QuirksUnleashed {
+    public static final String MOD_ID = "quirksunleashed";
+    public static final String MOD_NAME = "Quirks Unleashed";
     private static final Logger LOGGER = LoggerFactory.getLogger(MOD_NAME);
 
-    public NeoForgeTemplate(IEventBus modEventBus) {}
+    public static final int API_VERSION = 1;
+
+    public QuirksUnleashed(IEventBus modEventBus) {}
 
     public static ResourceLocation modLoc(String path) {
         return ResourceLocation.fromNamespaceAndPath(MOD_ID, path);
