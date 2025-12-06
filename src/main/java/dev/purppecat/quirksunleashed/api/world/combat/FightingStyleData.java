1f7f420fdd8090ec495fe80591d9f8936cb6294f
Admin <roeesalama@gmail.com>
2025-12-06 02:28:15 +0200

    Forgot these

---
+++ src/main/java/dev/purppecat/quirksunleashed/api/world/combat/FightingStyleData.java
@@ -0,0 +1,48 @@
+package dev.purppecat.quirksunleashed.api.world.combat;
+
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
+import dev.purppecat.quirksunleashed.QuirksUnleashed;
+import dev.thomasglasser.tommylib.api.util.TommyLibExtraStreamCodecs;
+import net.minecraft.network.RegistryFriendlyByteBuf;
+import net.minecraft.network.codec.ByteBufCodecs;
+import net.minecraft.network.codec.StreamCodec;
+import net.minecraft.resources.ResourceLocation;
+
+public record FightingStyleData(ResourceLocation punch1, ResourceLocation punch2, ResourceLocation punch3, ResourceLocation stance, ResourceLocation stanceSneak, ResourceLocation block, ResourceLocation counter, ResourceLocation heavy) {
+
+    public static final Codec<FightingStyleData> CODEC = RecordCodecBuilder.create(instance -> instance.group(
+            ResourceLocation.CODEC.fieldOf("punch1").forGetter(FightingStyleData::punch1),
+            ResourceLocation.CODEC.fieldOf("punch2").forGetter(FightingStyleData::punch2),
+            ResourceLocation.CODEC.fieldOf("punch3").forGetter(FightingStyleData::punch3),
+            ResourceLocation.CODEC.fieldOf("stance").forGetter(FightingStyleData::stance),
+            ResourceLocation.CODEC.fieldOf("stance_sneak").forGetter(FightingStyleData::stanceSneak),
+            ResourceLocation.CODEC.fieldOf("block").forGetter(FightingStyleData::block),
+            ResourceLocation.CODEC.fieldOf("counter").forGetter(FightingStyleData::counter),
+            ResourceLocation.CODEC.fieldOf("heavy").forGetter(FightingStyleData::heavy)).apply(instance, FightingStyleData::new));
+
+    public static final StreamCodec<RegistryFriendlyByteBuf, FightingStyleData> STREAM_CODEC = TommyLibExtraStreamCodecs.composite(
+            ByteBufCodecs.fromCodec(ResourceLocation.CODEC), FightingStyleData::punch1,
+            ByteBufCodecs.fromCodec(ResourceLocation.CODEC), FightingStyleData::punch2,
+            ByteBufCodecs.fromCodec(ResourceLocation.CODEC), FightingStyleData::punch3,
+            ByteBufCodecs.fromCodec(ResourceLocation.CODEC), FightingStyleData::stance,
+            ByteBufCodecs.fromCodec(ResourceLocation.CODEC), FightingStyleData::stanceSneak,
+            ByteBufCodecs.fromCodec(ResourceLocation.CODEC), FightingStyleData::block,
+            ByteBufCodecs.fromCodec(ResourceLocation.CODEC), FightingStyleData::counter,
+            ByteBufCodecs.fromCodec(ResourceLocation.CODEC), FightingStyleData::heavy,
+            FightingStyleData::new);
+    public FightingStyleData(ResourceLocation punch1, ResourceLocation punch2, ResourceLocation punch3, ResourceLocation stance, ResourceLocation stanceSneak, ResourceLocation block, ResourceLocation counter, ResourceLocation heavy) {
+        this.punch1 = punch1;
+        this.punch2 = punch2;
+        this.punch3 = punch3;
+        this.stance = stance;
+        this.stanceSneak = stanceSneak;
+        this.block = block;
+        this.counter = counter;
+        this.heavy = heavy;
+    }
+
+    public FightingStyleData() {
+        this(QuirksUnleashed.modLoc("punch"), QuirksUnleashed.modLoc("punch2"), QuirksUnleashed.modLoc("punch3"), QuirksUnleashed.modLoc("stance"), QuirksUnleashed.modLoc("stance_sneak"), QuirksUnleashed.modLoc("block"), QuirksUnleashed.modLoc("counter"), QuirksUnleashed.modLoc("heavy"));
+    }
+}
---
+++ src/main/java/dev/purppecat/quirksunleashed/api/world/combat/FightingStylesData.java
@@ -0,0 +1,67 @@
+package dev.purppecat.quirksunleashed.api.world.combat;
+
+import com.google.common.collect.Maps;
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
+import com.mojang.serialization.codecs.UnboundedMapCodec;
+import dev.purppecat.quirksunleashed.api.world.attachment.QuirksUnleashedAttachmentTypes;
+import dev.purppecat.quirksunleashed.impl.world.combat.FightingStyle;
+import it.unimi.dsi.fastutil.objects.Reference2ObjectOpenHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.function.BiConsumer;
+import net.minecraft.core.Holder;
+import net.minecraft.network.RegistryFriendlyByteBuf;
+import net.minecraft.network.codec.ByteBufCodecs;
+import net.minecraft.network.codec.StreamCodec;
+import net.minecraft.world.entity.Entity;
+
+public class FightingStylesData {
+    public static final UnboundedMapCodec<Holder<FightingStyle>, FightingStyleData> MAP_CODEC = Codec.unboundedMap(FightingStyle.CODEC, FightingStyleData.CODEC);
+    public static final Codec<FightingStylesData> CODEC = RecordCodecBuilder.create(instance -> instance.group(
+            MAP_CODEC.fieldOf("map").forGetter(set -> set.map)).apply(instance, FightingStylesData::new));
+    public static final StreamCodec<RegistryFriendlyByteBuf, FightingStylesData> STREAM_CODEC = StreamCodec.composite(
+            ByteBufCodecs.map(
+                    Maps::newHashMapWithExpectedSize,
+                    FightingStyle.STREAM_CODEC,
+                    FightingStyleData.STREAM_CODEC),
+            set -> set.map,
+            FightingStylesData::new);
+
+    private final Map<Holder<FightingStyle>, FightingStyleData> map;
+
+    public FightingStylesData() {
+        this.map = new Reference2ObjectOpenHashMap<>();
+    }
+
+    public FightingStylesData(Map<Holder<FightingStyle>, FightingStyleData> map) {
+        this.map = new Reference2ObjectOpenHashMap<>(map);
+    }
+
+    public FightingStyleData get(Holder<FightingStyleData> key) {
+        return map.getOrDefault(key, new FightingStyleData());
+    }
+
+    public FightingStyleData put(Entity entity, Holder<FightingStyle> key, FightingStyleData value, boolean syncToClient) {
+        FightingStyleData data = map.put(key, value);
+        save(entity);
+        return data;
+    }
+
+    public Set<Holder<FightingStyle>> keySet() {
+        return Set.copyOf(map.keySet());
+    }
+
+    public List<FightingStyleData> values() {
+        return List.copyOf(map.values());
+    }
+
+    public void forEach(BiConsumer<Holder<FightingStyle>, FightingStyleData> consumer) {
+        map.forEach(consumer);
+    }
+
+    public void save(Entity entity) {
+        entity.setData(QuirksUnleashedAttachmentTypes.FIGHTING_STYLES, this);
+    }
+}
